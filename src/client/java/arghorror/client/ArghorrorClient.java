64933732e290ff66c2c01f8054519b6ed91003e9
yesmcdaddy <petike200511@gmail.com>
2026-07-20 18:56:28 +0200

    Initial commit

---
+++ src/client/java/arghorror/client/ArghorrorClient.java
@@ -0,0 +1,10 @@
+package arghorror.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ArghorrorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
